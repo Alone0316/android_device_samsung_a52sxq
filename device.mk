cb36a76baf7b02035b864d80baa4298a65784444
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-04-25 20:56:35 -0400

    a52sxq: Uprev Bluetooth audio HIDL impl to 2.1
    Change-Id: I63211c9877c19d089e7cafa5a3cc52a888309dca

--- device.mk
+++ device.mk
@@ -65,7 +65,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libbthost_if \
     libbthost_if.vendor \
-    android.hardware.bluetooth.audio@2.0-impl \
+    android.hardware.bluetooth.audio@2.1-impl \
     android.hardware.bluetooth@1.0.vendor \
     com.qualcomm.qti.bluetooth_audio@1.0.vendor \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor
