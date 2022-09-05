342ddfa068af9081366b6f4d8cd8983da24b8f84
Alone0316 <rutujamandlik06@gmail.com>
2022-09-05 09:46:11 +0000

    a52sxq: Import modules.load from stock

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -70,6 +70,12 @@ TARGET_KERNEL_CONFIG := vendor/mesa_a52sxq_eur_open_defconfig
 TARGET_KERNEL_SOURCE := kernel/samsung/a52sxq
 TARGET_KERNEL_CLANG_COMPILE := true
 
+# Kernel modules
+BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load))
+BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load.recovery))
+BOOT_KERNEL_MODULES := $(BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD)
+TARGET_MODULE_ALIASES += wlan.ko:qca_cld3_wlan.ko
+
 # Metadata
 BOARD_USES_METADATA_PARTITION := true
 
