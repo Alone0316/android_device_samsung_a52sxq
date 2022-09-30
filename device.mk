606e6b68d08e66fe1f02db654839930ba6f6886d
Alone0316 <rutujamandlik06@gmail.com>
2022-09-30 06:15:09 +0000

    a52sxq: Build libavservices_minijail.so from source 01-08 14:32:40.346   901   901 F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/dspservice": library "libavservices_minijail.so" not found: needed by main executable

--- device.mk
+++ device.mk
@@ -86,6 +86,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.common-V1-ndk_platform
 
+# Minijail
+PRODUCT_PACKAGES += \
+    libavservices_minijail \
+    libavservices_minijail.vendor \
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
