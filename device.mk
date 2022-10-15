66b6a3ab862f8c135e02e14b2285f735897c4a6f
Demon000 <demonsingur@gmail.com>
2022-10-15 09:51:36 +0000

    a52sxq: build the WLAN stack from source

--- device.mk
+++ device.mk
@@ -364,6 +364,15 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.service.debuggable=1
 
 # Wifi
+PRODUCT_PACKAGES += \
+    android.hardware.wifi@1.0-service \
+    hostapd \
+    libwifi-hal-qcom \
+    libwpa_client \
+    wpa_cli \
+    wpa_supplicant \
+    wpa_supplicant.conf
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
