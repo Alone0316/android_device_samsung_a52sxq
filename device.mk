50ffc8a27b5f5ae4851a119b2e6ebed6e7c6fdd7
Alone0316 <rutujamandlik06@gmail.com>
2022-10-28 09:32:43 +0000

    a52sxq: Build additional radio libs

--- device.mk
+++ device.mk
@@ -348,6 +348,8 @@ PRODUCT_COPY_FILES += \
 
 # RIL
 PRODUCT_PACKAGES += \
+    android.hardware.radio@1.5.vendor \
+    android.hardware.radio.config@1.2.vendor \
     android.hardware.radio.deprecated@1.0 \
     android.hardware.radio.deprecated@1.0.vendor
 
