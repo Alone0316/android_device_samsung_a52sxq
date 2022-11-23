53bb29811880bdac1c0d872e6afdc3292124cc0a
Alone0316 <rutujamandlik06@gmail.com>
2022-11-23 09:12:10 +0000

    a52sxq: Switch to AOSP bt stack

--- bluetooth/include/bdroid_buildcfg.h
+++ bluetooth/include/bdroid_buildcfg.h
@@ -27,9 +27,8 @@
 
 #define BLE_VND_INCLUDED   TRUE
 #define MAX_ACL_CONNECTIONS   16
-#define MAX_L2CAP_CHANNELS    32
-#define GATT_MAX_PHY_CHANNEL  10
+#define MAX_L2CAP_CHANNELS    16
 
-#define AVDT_NUM_SEPS 35
+#define AVDT_NUM_SEPS 12
 
 #endif
