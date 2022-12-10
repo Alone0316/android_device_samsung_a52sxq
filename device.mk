8d0bde24dcf55a84b2437fffafba0a8f78c90cbe
Alone0316 <rutujamandlik06@gmail.com>
2022-12-10 07:21:05 +0000

    a52sxq: Import public.libraries.txt from stock

--- device.mk
+++ device.mk
@@ -276,6 +276,10 @@ PRODUCT_PACKAGES += \
 # Product characteristics
 PRODUCT_CHARACTERISTICS := nosdcard
 
+# Public libraries
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
+
 #No A/B
 AB_OTA_UPDATER := false
 
